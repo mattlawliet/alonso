3174915dc06cd0ad013a7e56e06f052b73ee63e6
mattlawliet <math970621@gmail.com>
2025-03-06 19:14:03 -0500

    Update config for build

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+python -m pip install --upgrade pip
 pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
 python manage.py collectstatic --noinput
